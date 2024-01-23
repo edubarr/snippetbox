69416258cf4ff4253ec0dd7a1df0ee0fe0d68a68
edubarr <eduaraujobarros@gmail.com>
2024-01-23 17:09:05 -0300

    refactor: move routes to separate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,24 +31,11 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Initialize a new ServeMux, and register all handlers to corresponding URL pattern.
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/".
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/get", app.getSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
 	// Initialize a new http.Server struct.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// Start a new web server on ":4000" and use the ServeMux as handler.
